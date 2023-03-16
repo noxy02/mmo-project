6c3a4c32b503473d8699cf35a40ca9967189bf69
crb637 <71448044+noxy02@users.noreply.github.com>
2023-03-16 17:04:42 +0700

    new safe spots
    add new safe spots for map1
    add map data for map2
    remove create name function

--- app.js
+++ app.js
@@ -30,6 +30,17 @@ const mapData = {
     "4x2": true,
     "3x2": true,
     "2x2": true,
+    "5x5": true,
+  },
+};
+
+const mapData2 = {
+  minX: 0,
+  maxX: 15,
+  minY: 0,
+  maxY: 15,
+  blockedSpaces: {
+
   },
 };
 
@@ -44,54 +55,25 @@ function getKeyString(x, y) {
   return `${x}x${y}`;
 }
 
-function createName() {
-  const prefix = randomFromArray([
-    "COOL",
-    "SUPER",
-    "HIP",
-    "SMUG",
-    "COOL",
-    "SILKY",
-    "GOOD",
-    "SAFE",
-    "DEAR",
-    "DAMP",
-    "WARM",
-    "RICH",
-    "LONG",
-    "DARK",
-    "SOFT",
-    "BUFF",
-    "DOPE",
-  ]);
-  const animal = randomFromArray([
-    "BEAR",
-    "DOG",
-    "CAT",
-    "FOX",
-    "LAMB",
-    "LION",
-    "BOAR",
-    "GOAT",
-    "VOLE",
-    "SEAL",
-    "PUMA",
-    "MULE",
-    "BULL",
-    "BIRD",
-    "BUG",
-  ]);
-  return `${prefix} ${animal}`;
-}
 
 function isSolid(x, y, pMap) {
-  const blockedNextSpace = mapData.blockedSpaces[getKeyString(x, y)];
+  if (pMap === 1) {
+    const blockedNextSpace = mapData.blockedSpaces[getKeyString(x, y)];
   return (
     blockedNextSpace ||
     x >= mapData.maxX ||
     x < mapData.minX ||
     (y < mapData.minY && pMap === 1)
   );
+  } else {
+    const blockedNextSpace = mapData2.blockedSpaces[getKeyString(x, y)];
+  return (
+    blockedNextSpace ||
+    x >= mapData.maxX ||
+    x < mapData.minX ||
+    (y < mapData.minY && pMap === 1)
+  );
+  }
   // return blockedNextSpace;
 }
 
@@ -107,29 +89,15 @@ function isBoundary(x, y) {
 function getRandomSafeSpot() {
   //We don't look things up by key here, so just return an x/y
   return randomFromArray([
-    { x: 1, y: 4 },
-    { x: 2, y: 4 },
-    { x: 1, y: 5 },
-    { x: 2, y: 6 },
-    { x: 2, y: 8 },
-    { x: 2, y: 9 },
-    { x: 4, y: 8 },
-    { x: 5, y: 5 },
-    { x: 5, y: 8 },
-    { x: 5, y: 10 },
-    { x: 5, y: 11 },
-    { x: 11, y: 7 },
+    { x: 1, y: 1 },
+    { x: 4, y: 6 },
+    { x: 2, y: 12 },
+    { x: 12, y: 12 },
     { x: 12, y: 7 },
-    { x: 13, y: 7 },
-    { x: 13, y: 6 },
-    { x: 13, y: 8 },
-    { x: 7, y: 6 },
-    { x: 7, y: 7 },
-    { x: 7, y: 8 },
-    { x: 8, y: 8 },
-    { x: 10, y: 8 },
-    { x: 8, y: 8 },
-    { x: 11, y: 4 },
+    { x: 12, y: 2 },
+    { x: 7, y: 4 },
+    { x: 6, y: 2 },
+    { x: 4, y: 3 },
   ]);
 }
 
@@ -354,15 +322,6 @@ function getRandomSafeSpot() {
       // }
     });
 
-    //Updates player name with text input
-    playerNameInput.addEventListener("change", (e) => {
-      const newName = e.target.value || createName();
-      playerNameInput.value = newName;
-      playerRef.update({
-        name: newName,
-      });
-    });
-
     //Update player color on button click
     playerColorButton.addEventListener("click", () => {
       const mySkinIndex = playerColors.indexOf(players[playerId].color);
@@ -381,14 +340,10 @@ function getRandomSafeSpot() {
       playerMap = 1;
       playerRef = firebase.database().ref(`players/${playerId}`);
 
-      const name = createName();
-      playerNameInput.value = name;
-
       const { x, y } = getRandomSafeSpot();
       // const { x, y } = { x: 7, y: 7 }; // for testing
       playerRef.set({
         id: playerId,
-        name,
         direction: "right",
         color: randomFromArray(playerColors),
         x,
